e4d2ecb316f488d5ab83e813d7adc1069448be89
Ann4017 <121239306+Ann4017@users.noreply.github.com>
2023-04-18 12:15:05 +0900

    commit

--- main.go
+++ main.go
@@ -6,65 +6,106 @@ import (
 	"os"
 )
 
-type custom_err struct {
+type password_err struct {
 	len int
 	std int
 }
 
-func main() {
-	err := scan_error("test")
-	if err != nil {
-		fmt.Println(err)
-	}
-}
+var (
+	open_err     = errors.New("file open error")
+	open_err_get = fmt.Errorf("발생한 오류: %w\n", open_err)
+)
 
-var new_err = errors.New("New error")
-
-func wrap_error() error {
-	w_err := fmt.Errorf("error: %w\n", new_err)
-
-	return w_err
-}
-
-func unwrap_error() error {
-	w_err := fmt.Errorf("error: %w\n", new_err)
-
-	unw_err := errors.Unwrap(w_err)
-	return unw_err
-}
-
-func scan_error(file_name string) error {
-
+func open_panic_test(file_name string) error {
 	file, err := os.Open(file_name)
 	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			return fmt.Errorf("same error: %w, %w", err, os.ErrNotExist)
-		}
-		return err
+		panic(open_err_get)
 	}
 	defer file.Close()
 
 	return nil
 }
 
-func (c custom_err) Error() string {
+func open_test(file_name string) error {
+	file, err := os.Open(file_name)
+	if err != nil {
+		if err == open_err {
+			//
+		}
+		return open_err_get
+	}
+	defer file.Close()
+
+	return nil
+}
+
+func (c password_err) Error() string {
 	return "암호 길이가 짧습니다."
 }
 
 func register_account(id, pwd string) error {
 	if len(pwd) < 10 {
-		return custom_err{len(pwd), 10}
+		return password_err{len(pwd), 10}
 	}
 	return nil
 }
 
-func conversion_err_type() {
-	err := register_account("abh4017", "12345")
-
+func main() {
+	//custom err
+	err := register_account("id", "pwd")
 	if err != nil {
-		if err_info, ok := err.(custom_err); ok {
-			fmt.Println(err_info.len)
+		if err_info, ok := err.(password_err); ok {
+			fmt.Printf("%s len: %d, std: %d\n", err_info, err_info.len, err_info.std)
 		}
-		fmt.Println(err)
 	}
+
+	//panic recover
+	func() {
+		err = open_panic_test("test")
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Printf("recover from: %v", r)
+			}
+		}()
+	}()
 }
+
+// var new_err = errors.New("New error")
+
+// func wrap_error() error {
+// 	w_err := fmt.Errorf("error: %w\n", new_err)
+
+// 	return w_err
+// }
+
+// func unwrap_error() error {
+// 	w_err := fmt.Errorf("error: %w\n", new_err)
+
+// 	unw_err := errors.Unwrap(w_err)
+// 	return unw_err
+// }
+
+// func scan_error(file_name string) error {
+
+// 	file, err := os.Open(file_name)
+// 	if err != nil {
+// 		if errors.Is(err, os.ErrNotExist) {
+// 			return fmt.Errorf("same error: %w, %w", err, os.ErrNotExist)
+// 		}
+// 		return err
+// 	}
+// 	defer file.Close()
+
+// 	return nil
+// }
+
+// func conversion_err_type() {
+// 	err := register_account("abh4017", "12345")
+
+// 	if err != nil {
+// 		if err_info, ok := err.(password_err); ok {
+// 			fmt.Println(err_info.len)
+// 		}
+// 		fmt.Println(err)
+// 	}
+// }
